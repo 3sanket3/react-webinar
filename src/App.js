4a27e2f0260a70f114b0ee82eaa34209f229dbc5
Sanket Patel <3sanket3@gmail.com>
2020-05-18 12:02:50 +0530

    add cricketer card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ms from "./ms.json";
+import chennaiCricketers from "./chennai.json";
+import mumbaiCricketers from "./mumbai.json";
+
+function CricketerCard(props) {
+  return (
+    <div className="card" key={props.key}>
+      <img
+        className="profile-pic"
+        src={props.cricketer.picture}
+        alt="profile pic"
+      ></img>
+
+      <span className="name">
+        {props.cricketer.name.first + " " + props.cricketer.name.last}{" "}
+      </span>
+      <span className="score">
+        {" "}
+        {props.cricketer.score} {props.cricketer.out ? null : "*"}{" "}
+      </span>
+    </div>
+  );
+}
 
 function App() {
+  console.log(ms);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Chennai Super Kings</h1>
+      <div className="team-container">
+        {chennaiCricketers.map((cricketer) => (
+          <CricketerCard key={cricketer.id} cricketer={cricketer} />
+        ))}
+      </div>
+
+      <h1>Mumbai Indians</h1>
+      <div className="team-container">
+        {mumbaiCricketers.map((cricketer) => (
+          <CricketerCard key={cricketer.id} cricketer={cricketer} />
+        ))}
+      </div>
     </div>
   );
 }
